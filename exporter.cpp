857b7d3f2d70d82c6f649d421b9f3a7679d98d20
Matt N <mattnappo@gmail.com>
2018-07-11 11:02:25 -0400

    THIS IS THE WORST JSON LIBRARY DONT USE IT

--- exporter.cpp
+++ exporter.cpp
@@ -9,33 +9,22 @@ using namespace rapidjson;
 using namespace std;
 
 string importer() {
-  ifstream t("blockchains/blockchain_0.json");
+  ifstream t("blockchains/blockchain_2.json");
   string RAW((istreambuf_iterator<char>(t)), istreambuf_iterator<char>());
   return RAW;
 }
 int main() {
-  const string RAW_JSON = importer();
-  cout << RAW_JSON << endl;
-  const char *json = RAW_JSON.c_str();
-  // const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  const char *json = importer().c_str();
+  // const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  cout << importer() << endl;
+
   Document document;
   document.Parse(json);
 
-  Value &blocks = document["blocks"];
-  for (SizeType i = 0; i < blocks.Size(); i++){
-    CCLOG("{x=%f, y=%f}", blocks[i]["x"].GetDouble(), blocks[i]["y"].GetDouble());
-  }
 
-  StringBuffer block_buffer;
-  Writer<StringBuffer> block_writer(block_buffer);
-  blocks.Accept(block_writer);
-  cout << "Block0: " << block_buffer.GetString() << endl;
+  assert(document.IsObject());
 
-/*
-  StringBuffer buffer;
-  Writer<StringBuffer> writer(buffer);
-  document.Accept(writer);
-  cout << buffer.GetString() << endl;
-  return 0;
-*/
+  assert(document.HasMember("hello"));
+assert(document["hello"].IsString());
+printf("hello = %s\n", document["hello"].GetString());
 }
